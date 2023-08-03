de63ed00a9444c9dfb6d92f4ec054d23ce59b67a
Codehash001 <codehashblockchain@gmail.com>
2023-08-03 10:10:53 +0530

    env update

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,5 +1,10 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Supabase URL and key must be defined in the environment variables');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
